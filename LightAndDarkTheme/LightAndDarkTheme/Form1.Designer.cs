66d7942256718b8196573baa411a2759f5c2c417
semihceltik <56488622+semihceltik@users.noreply.github.com>
2021-07-16 21:43:32 +0300

    Add files via upload

---
+++ LightAndDarkTheme/LightAndDarkTheme/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace LightAndDarkTheme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.darktheme = new System.Windows.Forms.CheckBox();
+            this.lighttheme = new System.Windows.Forms.CheckBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(173, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1128, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THIS IS YOUR AUTOMATION SYSTEM INTERFACE";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(1210, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 111);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EXCEPT";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(978, 526);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 111);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "BUTTON2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(738, 526);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 111);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "BUTTON3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(1280, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "CHANGE THEME";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.darktheme);
+            this.panel1.Controls.Add(this.lighttheme);
+            this.panel1.Location = new System.Drawing.Point(1074, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // darktheme
+            // 
+            this.darktheme.AutoSize = true;
+            this.darktheme.Location = new System.Drawing.Point(3, 56);
+            this.darktheme.Name = "darktheme";
+            this.darktheme.Size = new System.Drawing.Size(84, 19);
+            this.darktheme.TabIndex = 1;
+            this.darktheme.Text = "Darktheme";
+            this.darktheme.UseVisualStyleBackColor = true;
+            this.darktheme.CheckedChanged += new System.EventHandler(this.darktheme_CheckedChanged);
+            // 
+            // lighttheme
+            // 
+            this.lighttheme.AutoSize = true;
+            this.lighttheme.Location = new System.Drawing.Point(3, 12);
+            this.lighttheme.Name = "lighttheme";
+            this.lighttheme.Size = new System.Drawing.Size(92, 19);
+            this.lighttheme.TabIndex = 0;
+            this.lighttheme.Text = "Light Theme";
+            this.lighttheme.UseVisualStyleBackColor = true;
+            this.lighttheme.CheckedChanged += new System.EventHandler(this.lighttheme_CheckedChanged);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button5.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(161, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(36, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "X";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 649);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox darktheme;
+        private System.Windows.Forms.CheckBox lighttheme;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ LightAndDarkTheme/LightAndDarkTheme/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightAndDarkTheme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible = false; // I closed theme panel cause i don't want to show this when program is starting.
+            
+            // U can use this in different forms. 
+            // U can change ur background image,fonts,backcolors,events bla bla bla...
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true; 
+        }
+
+        private void darktheme_CheckedChanged(object sender, EventArgs e)
+            // DARKTHEME AREA
+        {
+            
+            lighttheme.Checked = false; // If darktheme is checked == true lighttheme.checked == false...
+            this.BackColor = Color.Black;
+            button1.ForeColor = Color.White;
+            button2.ForeColor = Color.White;
+            button3.ForeColor = Color.White;
+            label1.ForeColor = Color.White;
+            darktheme.ForeColor = Color.White;
+            lighttheme.ForeColor = Color.White;
+        }
+
+        private void lighttheme_CheckedChanged(object sender, EventArgs e)
+        {
+            // LIGHT THEME AREA
+            darktheme.Checked = false;
+            
+            this.BackColor = Color.White;
+            button1.ForeColor = Color.Black; 
+            button2.ForeColor = Color.Black;
+            button3.ForeColor = Color.Black;
+            label1.ForeColor = Color.Black;
+            darktheme.ForeColor = Color.Black;
+            lighttheme.ForeColor = Color.Black;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
